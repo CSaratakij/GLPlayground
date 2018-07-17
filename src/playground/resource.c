06bc4e80b8c0a84a5c3bc1198c72b32cc7b4d377
Chatchai Saratakij <CSaratakij@gmail.com>
2018-07-17 18:04:27 +0700

    Remove unnecessary header from source

--- src/playground/resource.c
+++ src/playground/resource.c
@@ -1,7 +1,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "playground/resource.h"
 
 char* LoadFile(char* path)
 {
--- src/playground/ui.c
+++ src/playground/ui.c
@@ -3,7 +3,6 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include "playground/ui.h"
 
 void SetupUI(GLFWwindow* window)
 {
